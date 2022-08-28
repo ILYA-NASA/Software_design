09841e7ba42d56e9bb28595ded20d99090ae9eca
Илья Насалевич <il.nasa@outlook.com>
2022-08-28 14:00:29 +0300

    указал корректный путь для импорта

--- Lecture_02/Ex003/Program.java
+++ Lecture_02/Ex003/Program.java
@@ -4,8 +4,8 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
-import Lesson_08.Ex003.alliance.*;
-import Lesson_08.Ex003.*;
+import Lecture_02.Ex003.alliance.*;
+import Lecture_02.Ex003.*;
 
 public class Program {
     public static void main(String[] args) {

351a799622ea56682ccf570865d1ab6f1c2344d4
Илья Насалевич <il.nasa@outlook.com>
2022-08-30 14:38:01 +0300

    добавил словарь с именами персонажей

---
+++ Home_01/Program.java
@@ -0,0 +1,17 @@
+package Home_01;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Program {
+    public static void main(String[] args) {
+        Map<String, String> FellowshipOfRing = new HashMap<>();
+        FellowshipOfRing.put("Peasant", "Frodo");
+        FellowshipOfRing.put("Outlaw", "Sauron");
+        FellowshipOfRing.put("Sniper", "Legolas");
+        FellowshipOfRing.put("Magician", "Gandalf");
+        FellowshipOfRing.put("Spearman", "Gimli");
+        FellowshipOfRing.put("Crossbowman", "Aragorn");
+        FellowshipOfRing.put("Monk", "Arwen");
+    }
+}

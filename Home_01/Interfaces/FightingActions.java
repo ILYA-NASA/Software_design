1f5e53398f27b32594afdc158a130f9beff9f53d
Илья Насалевич <il.nasa@outlook.com>
2022-08-31 21:05:06 +0300

    интерфейс атаки

---
+++ Home_01/Interfaces/FightingActions.java
@@ -0,0 +1,13 @@
+package Home_01.Interfaces;
+
+/**
+ * 
+ * FightingActions
+ */
+public interface FightingActions {
+    int hit();
+    int getDamaged();
+    boolean status();
+    boolean moved();
+    String condition();
+}

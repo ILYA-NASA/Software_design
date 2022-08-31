6facf553beb654a57208af4e4a0dcc409d43fe5e
Илья Насалевич <il.nasa@outlook.com>
2022-08-31 10:38:21 +0300

    заготовки для метода который будет сравнивать словарь со списком и возвращать значения в виде имен героев

--- Home_01/Program.java
+++ Home_01/Program.java
@@ -1,24 +1,33 @@
 package Home_01;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class Program {
     public static void main(String[] args) {
-        Map<String, String> FellowshipOfRing = new HashMap<>();
-        FellowshipOfRing.put("Peasant", "Frodo");
-        FellowshipOfRing.put("Outlaw", "Sauron");
-        FellowshipOfRing.put("Sniper", "Legolas");
-        FellowshipOfRing.put("Magician", "Gandalf");
-        FellowshipOfRing.put("Spearman", "Gimli");
-        FellowshipOfRing.put("Crossbowman", "Aragorn");
-        FellowshipOfRing.put("Monk", "Arwen");
+        Map<String, String> fellowshipOfRing = new HashMap<>();
+        fellowshipOfRing.put("Peasant", "Frodo");
+        fellowshipOfRing.put("Outlaw", "Sauron");
+        fellowshipOfRing.put("Sniper", "Legolas");
+        fellowshipOfRing.put("Magician", "Gandalf");
+        fellowshipOfRing.put("Spearman", "Gimli");
+        fellowshipOfRing.put("Crossbowman", "Aragorn");
+        fellowshipOfRing.put("Monk", "Arwen");
 
-        // String keyNameHero = "Crossbowman";
-        // if(FellowshipOfRing.containsKey(keyNameHero)){
-        //     new Crossbowman(FellowshipOfRing.get(keyNameHero));
-        // }
-        
+        List<String> keyNameHeroList = new ArrayList<>();
+        keyNameHeroList.add("Crossbowman");
+        keyNameHeroList.add("Outlaw");
+        keyNameHeroList.add("Sniper");
+        keyNameHeroList.add("Magician");
+        keyNameHeroList.add("Spearman");
+        keyNameHeroList.add("Crossbowman");
+        keyNameHeroList.add("Monk");
+
+        // String nameHero = fellowshipOfRing.getOrDefault(keyNameHeroList.get(1), "Имя не задано");
+        // new Crossbowman(nameHero);
+
         new Crossbowman();
         System.out.println(Crossbowman.getInfo());
     }

6ca5332f841ea301c69c407571b3129836e21aec
Илья Насалевич <il.nasa@outlook.com>
2022-09-06 15:04:47 +0300

    новые методы формирования команд и вывода информации в консоль

--- Home_01/Program.java
+++ Home_01/Program.java
@@ -1,9 +1,96 @@
 package Home_01;
 
+import Home_01.Abstracts.BaseHero;
 import Home_01.Characters.*;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.Scanner;
+
 public class Program {
+
     public static void main(String[] args) {
-        System.out.println(new Crossbowman().info());
+        int step = 0;
+
+        List<BaseHero> mordor = new ArrayList<>();
+        List<BaseHero> isengard = new ArrayList<>();
+
+        mordor.add(new Peasant(mordor, 0, 0));
+        mordor.add(new Outlaw(mordor, 0, 0));
+        mordor.add(new Sniper(mordor, 0, 0));
+
+        Random rnd = new Random();
+        for (int i = 0; i < 5; i++) {
+            switch (rnd.nextInt(4)) {
+                case 0:
+                    mordor.add(new Peasant(mordor, 0, 0));
+                    break;
+                case 1:
+                    mordor.add(new Outlaw(mordor, 0, 0));
+                    break;
+                case 2:
+                    mordor.add(new Sniper(mordor, 0, 0));
+                    break;
+                default:
+                    mordor.add(new Magician(mordor, 0, 0));
+            }
+        }
+
+        isengard.add(new Monk(isengard, 0, 0));
+        isengard.add(new Crossbowman(isengard, 0, 0));
+        isengard.add(new Spearman(isengard, 0, 0));
+
+        for (int i = 0; i < 5; i++) {
+            switch (rnd.nextInt(4)) {
+                case 0:
+                    isengard.add(new Monk(isengard, 0, 0));
+                    break;
+                case 1:
+                    isengard.add(new Crossbowman(isengard, 0, 0));
+                    break;
+                case 2:
+                    isengard.add(new Spearman(isengard, 0, 0));
+                    break;
+                default:
+                    isengard.add(new Magician(isengard, 0, 0));
+            }
+        }
+
+        if (step == 0) {
+            System.out.println("First step");
+        } else {
+            System.out.println("Step:" + Integer.toString(step));
+        }
+        System.out.println("--------------------------------------------");
+        System.out.println("Dark side\t\t\t\t\t\t\tWhite side");
+        for (int i = 0; i < mordor.size(); i++) {
+            if (mordor.get(i).returnCondition().length() > 27) {
+                System.out.println(mordor.get(i).returnCondition() + "\t:\t" + isengard.get(i).returnCondition());
+            } else {
+                System.out.println(mordor.get(i).returnCondition() + "\t\t:\t" + isengard.get(i).returnCondition());
+            }
+        }
+        Scanner in = new Scanner(System.in);
+        while (true) {
+            String txt = in.nextLine();
+            if (txt.equals("next")) {
+                for (int i = 0; i < mordor.size(); i++) {
+                    mordor.get(i).step();
+                    isengard.get(i).step();
+                    if (mordor.get(i).returnCondition().length() > 27) {
+                        System.out.println(
+                                mordor.get(i).returnCondition() + "\t:\t" + isengard.get(i).returnCondition());
+                    } else {
+                        System.out.println(
+                                mordor.get(i).returnCondition() + "\t\t:\t" + isengard.get(i).returnCondition());
+                    }
+                }
+            }
+            if (txt.equals("quit")) {
+                break;
+            }
+        }
+
     }
 }

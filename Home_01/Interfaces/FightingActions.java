ea7a0abc39e9db4e7f97b5eff15a12aaea858bb7
Илья Насалевич <il.nasa@outlook.com>
2022-09-06 15:03:25 +0300

    оптимальный набор поведения сущностей (как в примере из семинара)

--- Home_01/Interfaces/FightingActions.java
+++ Home_01/Interfaces/FightingActions.java
@@ -1,13 +1,7 @@
 package Home_01.Interfaces;
 
-/**
- * 
- * FightingActions
- */
 public interface FightingActions {
-    int makeAttack();
-    int acceptDamaged();
     boolean status();
-    boolean moved();
-    String condition();
+    void step();
+    String returnCondition();
 }

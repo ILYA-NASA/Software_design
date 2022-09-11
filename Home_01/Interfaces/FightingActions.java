51afdf137b4161c8fd46d26d511b9068e2d23f5d
Илья Насалевич <il.nasa@outlook.com>
2022-09-11 19:39:16 +0300

    вернул исходное поведение

--- Home_01/Interfaces/FightingActions.java
+++ Home_01/Interfaces/FightingActions.java
@@ -1,7 +1,11 @@
 package Home_01.Interfaces;
 
+import Home_01.Characters.Vector2;
+
 public interface FightingActions {
     boolean status();
     void step();
     String returnCondition();
+    String getName();
+    Vector2 getPosition();
 }

9c3c891848e2a65161043ec432f3efc2953f94de
Илья Насалевич <il.nasa@outlook.com>
2022-09-11 19:37:48 +0300

    логика шагов

--- Home_01/Abstracts/BaseHero.java
+++ Home_01/Abstracts/BaseHero.java
@@ -18,18 +18,37 @@ public abstract class BaseHero implements FightingActions {
     protected String name;
     protected String status;
     protected Vector2 position;
-
-    protected List<BaseHero> getList() {
-        return list;
-    }
-
     protected List<BaseHero> list;
 
-    public BaseHero(List<BaseHero> side) {
-        list = side;
-    }
-
+    public BaseHero(List<BaseHero> side){list = side;}
+    @Override
     public String returnCondition() {
-        return name + " H:" + crntHealth + " D:" + defence + " A:" + attack + " " + status;
+        return name +
+                " H:" + crntHealth +
+                " D:" + defence +
+                " A:" + attack +
+                " Dmg:" + (int)(Math.abs((damage.x+damage.y)/2)) +
+                (shoot>0?" Shots:" + shoot:"") + " " +
+                status;
+    }
+    @Override
+    public void step() {}
+    @Override
+    public String getName() {return name;}
+    @Override
+    public Vector2 getPosition() {return position;}
+    public Vector2 getDistance(List<BaseHero> side){
+        int dist = Integer.MAX_VALUE;
+        int out = 0;
+        for (int i = 0; i < side.size(); i++) {
+            float dX = Math.abs(side.get(i).position.x - position.x);
+            float dY = Math.abs(side.get(i).position.y - position.y);
+            long tD = Math.round(Math.sqrt(dX*dX + dY*dY));
+            if (dist > tD) {
+                out = i;
+                dist = (int) tD;
+            }
+        }
+        return new Vector2(out, dist);
     }
 }

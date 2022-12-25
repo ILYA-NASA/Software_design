3c1c2f246c3a28e834a18b949ef7d823477b7528
Илья Насалевич <99810114+ILYA-NASA@users.noreply.github.com>
2022-12-25 20:14:59 +0300

    Open-closed

--- SOLID/Lecture_06/Ex002_2OCP.java
+++ SOLID/Lecture_06/Ex002_2OCP.java
@@ -1,6 +1,5 @@
 public class Ex002_2OCP {
     public static void main(String[] args) {
-
         System.out.println(new BaseClient().getDiscount(100));
         System.out.println(new SilverPartner().getDiscount(100));
         System.out.println(new VIPPartner().getDiscount(100));

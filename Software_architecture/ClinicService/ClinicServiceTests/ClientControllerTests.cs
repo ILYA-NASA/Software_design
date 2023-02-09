1c2c1bb0e175c2ac1d7376f5e7516cd14dce09bc
ILYA.NASA <il.nasa@outlook.com>
2023-02-09 19:44:14 +0300

    xUnit Test Project

---
+++ Software_architecture/ClinicService/ClinicServiceTests/ClientControllerTests.cs
@@ -0,0 +1,55 @@
+﻿using ClinicService.Controllers;
+using ClinicService.Models;
+using ClinicService.Services;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClinicServiceTests
+{
+    public class ClientControllerTests
+    {
+
+        private ClientController _clientController;
+        private Mock<IClientRepository> _mockClientRepository;
+
+        public ClientControllerTests()
+        {
+            _mockClientRepository = new Mock<IClientRepository>();
+            _clientController = new ClientController(_mockClientRepository.Object);
+
+        }
+
+        [Fact]
+        public void GetAllClientsTest()
+        {
+            // [1] Подготовка данных для тестирования
+            _mockClientRepository.Setup(repository =>
+                repository.GetAll()).Returns(new List<Client>()
+                {
+                    new Client(),
+                    new Client(),
+                    new Client()
+                });
+
+            ActionResult<IList<Client>>  operationResult = _clientController.GetAll();
+
+
+            Assert.IsType<OkObjectResult>(operationResult.Result);
+
+            _mockClientRepository.Verify(repository => repository.GetAll(), Times.Once);
+
+            Assert.IsAssignableFrom<IList<Client>>(((OkObjectResult)operationResult.Result).Value);
+
+
+        }
+
+
+
+
+    }
+}
---
+++ Software_architecture/ClinicService/ClinicServiceTests/PetControllerTests.cs
@@ -0,0 +1,111 @@
+﻿using ClinicService.Controllers;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClinicServiceTests
+{
+    public class PetControllerTests
+    {
+        private PetController _petController;
+
+        public PetControllerTests()
+        {
+            _petController = new PetController();
+        }
+
+        [Fact]
+        public void PetCreateBadRequestTest()
+        {
+            CreatePetRequest createPetRequest = new CreatePetRequest();
+            createPetRequest.Birthday = DateTime.Now.AddYears(-35);
+            createPetRequest.ClientId = 1;
+
+
+            ActionResult<int> operationResult = _petController.Create(createPetRequest);
+
+            int expectedOperationValue = 0;
+
+            Assert.IsType<BadRequestObjectResult>(operationResult.Result);
+            Assert.Equal<int>(expectedOperationValue, (int)((BadRequestObjectResult)operationResult.Result).Value);
+        }
+
+        [Fact]
+        public void PetCreateTest()
+        {
+            // МЕТОД ТЕСТИРОВАНИЯ СОСТОИТ ИЗ УСЛОВНЫХ 3 ЧАСТЕЙ
+
+            // [1] Подготовка данных для тестирования
+            CreatePetRequest createPetRequest = new CreatePetRequest();
+            createPetRequest.Name = "Персик";
+            createPetRequest.Birthday = DateTime.Now.AddYears(-15);
+            createPetRequest.ClientId = 1;
+
+
+            // [2] Исполнение тестируемого метода
+            ActionResult<int> operationResult =  _petController.Create(createPetRequest);
+
+            // [3] Подготовка эталонного результата (expected), проверка результата
+            int expectedOperationValue = 1;
+
+
+            Assert.IsType<OkObjectResult>(operationResult.Result);
+            Assert.Equal<int>(expectedOperationValue, (int)((OkObjectResult)operationResult.Result).Value);
+
+        }
+
+        [Theory]
+        [InlineData(0)]
+        [InlineData(-10)]
+        [InlineData(-1000)]
+        [InlineData(-54)]
+        public void DeletePetBadRequestTest(int petId)
+        {
+            // [2] Исполнение тестируемого метода
+            ActionResult<int> operationResult = _petController.Delete(petId);
+
+            // [3] Подготовка эталонного результата (expected), проверка результата
+            int expectedOperationValue = 0;
+
+
+            Assert.IsType<BadRequestObjectResult>(operationResult.Result);
+            Assert.Equal<int>(expectedOperationValue, (int)((BadRequestObjectResult)operationResult.Result).Value);
+
+        }
+
+        [Theory]
+        [InlineData(1)]
+        [InlineData(10)]
+        [InlineData(1000)]
+        [InlineData(54)]
+        public void DeletePetTest(int petId)
+        {
+            // [2] Исполнение тестируемого метода
+            ActionResult<int> operationResult = _petController.Delete(petId);
+
+            // [3] Подготовка эталонного результата (expected), проверка результата
+            int expectedOperationValue = 1;
+
+
+            Assert.IsType<OkObjectResult>(operationResult.Result);
+            Assert.Equal<int>(expectedOperationValue, (int)((OkObjectResult)operationResult.Result).Value);
+
+        }
+
+
+
+    }
+
+    class BaseClass
+    {
+
+    }
+
+    class MyClass : BaseClass
+    {
+
+    }
+}
